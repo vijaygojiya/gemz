d0e2a299a230aaabf50623775067d1a63a5c2364
Krish-Parekh <krishparekh303@gmail.com>
2023-11-20 13:40:22 +0530

    Fix login form clearing credentials

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -33,7 +33,7 @@ export default function Login() {
 
   const { trigger } = useAuthServerMutation("/login", {
     onSuccess: (data) => {
-      console.log("Login success: ", data);
+      setCredentials({ username: "", password: "" });
     },
   });
 
