9f6925b39feae82443101fcd98a9475a450feeec
Aman-ethan <aman@ethan-ai.com>
2023-11-29 15:06:24 +0530

    Added-lint

--- components/Main/Overview/Vaultz/Vaultz.tsx
+++ components/Main/Overview/Vaultz/Vaultz.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { Text } from "@gluestack-ui/themed";
+import { VStack } from "@gluestack-ui/themed";
+import Header from "./common/Header";
+import VaultzList from "./common/VaultzList";
 
 export default function Vaultz() {
-  return <Text>Family investments</Text>;
+  return <VStack space="md">
+    <Header heading={"Recently Added"} />
+    <VaultzList />
+  </VStack>
 }
