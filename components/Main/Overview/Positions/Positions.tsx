52b94e980a61ce4b9bde3e41fc8ce1f8d9ebf38c
Adithya Suresh Nair <75528544+adithyanair11@users.noreply.github.com>
2023-11-29 14:40:35 +0530

    added positions page (#13)
    Co-authored-by: Prakash <prakash@Amits-MacBook-Pro.local>

--- components/Main/Overview/Positions/Positions.tsx
+++ components/Main/Overview/Positions/Positions.tsx
@@ -1,8 +1,27 @@
-import React from "react"
-import { Text } from "@gluestack-ui/themed"
+import React from "react";
+import { Heading, VStack, Text } from "@gluestack-ui/themed";
+import { SafeAreaView } from "react-native";
+import Colors from "../../../../constants/Colors";
+import PositionList from "./common/PositionList";
 
-export default function Positions(){
-    return(
-        <Text>Positions</Text>
-    )
+export interface IPositionsCard {
+  id: number;
+  security_name: string;
+  ticker: string;
+  percentage: string;
+  market_value: string;
+}
+
+export default function Positions() {
+  return (
+    <SafeAreaView>
+      <VStack space="md">
+        <Heading>Positions</Heading>
+        <Text size="3xl" color={Colors.dark}>
+          $525,193.06
+        </Text>
+        <PositionList />
+      </VStack>
+    </SafeAreaView>
+  );
 }
