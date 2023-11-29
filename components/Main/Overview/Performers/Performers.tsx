d4e11eafe4cd4e312e04919198a4a293877e50c8
Prakash <prakash@Amits-MacBook-Pro.local>
2023-11-29 09:07:49 +0530

    adding gainers and losers

--- components/Main/Overview/Performers/Performers.tsx
+++ components/Main/Overview/Performers/Performers.tsx
@@ -1,56 +1,36 @@
 import React, { useState } from "react";
-import { Text, View } from "@gluestack-ui/themed";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import Colors from "../../../../constants/Colors";
+import { Text, VStack } from "@gluestack-ui/themed";
+import { SafeAreaView } from "react-native";
+import PerformerSwitch from "./common/PerformerSwitch";
+import PerformerList from "./common/PerformerList";
 
-type TPerformer = "Gainer" | "Loser";
-
-export default function Performers() {
-  const [activeTab, setActiveTab] = useState<TPerformer>("Gainer");
-
-  function handleTabPress(tabName: TPerformer) {
-    setActiveTab(tabName);
-  }
-
-  const getTabStyles = (tabName: TPerformer) => ({
-    ...styles.tab,
-    ...(activeTab === tabName && styles.activeTab),
-    backgroundColor: activeTab === tabName ? Colors.primary : undefined,
-  });
-
-  return (
-    <>
-      <View flexDirection="row" justifycontent="center" alignItems="center">
-        <TouchableOpacity
-          style={getTabStyles("Gainer")}
-          onPress={() => handleTabPress("Gainer")}
-        >
-          <Text color={activeTab === "Gainer" ? "#fff" : Colors.primary}>
-            Gainers
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={getTabStyles("Loser")}
-          onPress={() => handleTabPress("Loser")}
-        >
-          <Text color={activeTab === "Loser" ? "#fff" : Colors.primary}>
-            Losers
-          </Text>
-        </TouchableOpacity> 
-      </View>
-    </>
-  );
+export interface IPerformerCard {
+  id: number;
+  name: string;
+  percentage: string;
+  total_pl: string;
 }
 
-const styles = StyleSheet.create({
-  tab: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    height: 50,
-    borderRadius: 10,
-  },
-  activeTab: {
-    backgroundColor: Colors.primary,
-  },
-});
+export default function Performers() {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const handleSelectedTab = (selectedTab: number) => {
+    setSelectedTab(selectedTab);
+  };
+
+  return (
+    <VStack space="md">
+      <PerformerSwitch
+        selectedTab={selectedTab}
+        setSelectedTab={handleSelectedTab}
+      />
+      {selectedTab === 0 ? (
+        <Text>Top Gain: 20.74K</Text>
+      ) : (
+        <Text>Top Loss: -809.72K</Text>
+      )}
+      <SafeAreaView style={{ flex: 1 }}>
+        <PerformerList selectedTab={selectedTab} />
+      </SafeAreaView>
+    </VStack>
+  );
+}
